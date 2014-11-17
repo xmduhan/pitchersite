2c839669e03b52e058658a8fda90287b80050169
xmduhan <xmduhan@gmail.com>
2014-11-17 21:48:34 +0800

    ...

--- pitcher/admin.py
+++ pitcher/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+
+class TicketCountLogAdmin(admin.ModelAdmin):
+    fields = [
+        'logTime', 'seq', 'departure', 'arrival', 'flightCode', 'departureTime', 'price', 'ticketCount', 'flightId'
+    ]
+    list_display = (
+    'logTime', 'seq', 'departure', 'arrival', 'flightCode', 'departureTime', 'price', 'ticketCount', 'flightId')
+
+
+admin.site.register(TicketCountLog, TicketCountLogAdmin)
