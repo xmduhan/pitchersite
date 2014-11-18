db58afefca3190653f265ef534cdd3858afa5298
xmduhan <xmduhan@gmail.com>
2014-11-18 16:42:54 +0800

    ....

--- run.py
+++ run.py
@@ -19,32 +19,44 @@ from pitcher.models import TicketCountLog
 from ticketpitcher import pitcher
 
 
-#%% 登录
-pitcher.login('xmjf001','123456')
+#%% 不停的刷新票的数量信息
+username = 'xmjf001'
+password = '123456'
+day = '2014-12-08'
 
+#%%
+def loopPitcher():
+    while pitcher.isLogin():
+        ticketInfo = pitcher.getTicketInfo(day)
+        for i in range(len(ticketInfo.index)):
+            row = ticketInfo.irow(i)
+            log = TicketCountLog()
+            log.seq = row[u'序号']
+            log.departure = row[u'出发码头']
+            log.arrival = row[u'抵达码头']
+            log.flightCode = row[u'航班号']
+            log.departureTime = row[u'开航时间']
+            log.price = row[u'票价']
+            log.ticketCount = row[u'余票']
+            log.flightId = row[u'航班ID']
+            log.save()
+        #print u'成功读取数据,等待5秒……'
+        print u'Read data successfully,waiting 5 seconds ... ...'
+        time.sleep(5)
+    else:
+        raise Exception(u'Login losted!Will relogin.')
 
+#%%
+while True:
+    try:
+        pitcher.login(username,password)
+        loopPitcher()
+    except Exception as e :
+        print e
+    print u'Waiting 10 seconds ... ...'
+    time.sleep(10)
 
-#%% 不停的刷新票的数量信息 
-day = '2014-11-29'
-while pitcher.isLogin():
-    ticketInfo = pitcher.getTicketInfo(day)
-    for i in range(len(ticketInfo.index)):
-        row = ticketInfo.irow(i)
-        log = TicketCountLog() 
-        log.seq = row[u'序号']
-        log.departure = row[u'出发码头']
-        log.arrival = row[u'抵达码头']  
-        log.flightCode = row[u'航班号']
-        log.departureTime = row[u'开航时间']
-        log.price = row[u'票价']
-        log.ticketCount = row[u'余票']
-        log.flightId = row[u'航班ID']
-        log.save()
-    #print u'成功读取数据,等待5秒……'
-    print u'read data successfully,waiting 5 seconds ... ...'
-    time.sleep(5)
-
-
+#%%
 #select * from pitcher_ticketcountlog
 #where "departureTime" = '09:30'
 #  and "departure" = '邮轮中心厦鼓码头'
