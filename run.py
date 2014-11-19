a957632f0f1006283538f18c29246de5b96d7996
xmduhan <xmduhan@gmail.com>
2014-11-19 10:46:29 +0800

    ....

--- run.py
+++ run.py
@@ -77,10 +77,10 @@ while True:
 #  order by "logTime";
 
 
-#select  "departureTime",count(*)*3 机会时间
+#select  "departureTime",count(*)*3 "机会时间(秒)"
 #from pitcher_ticketcountlog
 #where 1 = 1
-#  and "departureTime" >= '08:10' and "departureTime" <= '10:10'
+#  and "departureTime" >= '08:10' and "departureTime" <= '10:30'
 #  and "departure" = '邮轮中心厦鼓码头'
 #  and "arrival" = '三丘田码头'
 #  and "ticketCount" > 0
