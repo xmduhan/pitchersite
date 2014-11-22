bd6f768247c096758e56dde8251292d37360c91a
xmduhan <xmduhan@gmail.com>
2014-11-22 09:12:42 +0800

    ...

--- pitcher/tasks.py
+++ pitcher/tasks.py
@@ -1,19 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
-from celery import shared_task
-
-
-@shared_task
+from __future__ import absolute_import 
+import time
+from pitchersite.celery import app
+@app.task
 def add(x, y):
-    return x + y
+       time.sleep(10)
+       return x + y
 
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
--- pitchersite/celery.py
+++ pitchersite/celery.py
@@ -13,6 +13,10 @@ app = Celery('pitchersite')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.conf.update(
+    CELERY_RESULT_BACKEND='djcelery.backends.database:DatabaseBackend',
+)
+
 
 @app.task(bind=True)
 def debug_task(self):
