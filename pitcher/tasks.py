4e39c4f2201c6829e98434680e27372038d45aa4
xmduhan <xmduhan@gmail.com>
2014-11-24 21:38:40 +0800

    ...

--- pitcher/tasks.py
+++ pitcher/tasks.py
@@ -96,6 +96,8 @@ def orderTicket(dailyFlightId, n):
 #%% 设置全局变量
 normalWaitingSecond = 1
 errorWaitingSecond = 3
+maxLoginTry = 5
+timeToStop = '12:00'
 # 读取信息信息
 systemConfig = SystemConfig.objects.all()[0]
 username = systemConfig.username
@@ -153,40 +155,50 @@ def pitchLoop():
         ticketInfo = getTicketInfo(day)
         if len(ticketInfo.index):
             # 已经放票了,根据配置数据开始顺序进行抢票
+            writeSystemLog(u'系统已经放票，开始要抢票...')
             pitchConfig = getPitchConfig()
             for i in range(len(pitchConfig.index)):
                 # 读取当前记录要抢的航班号和需票信息
                 flightCode = pitchConfig['flightCode']
                 need = pitchConfig['need']
+                ItemMessage = u'尝试抢票(航班号:%s,需票数:%d):' % (flightCode, need)
+                writeSystemLog(ItemMessage + u'开始!')
                 # 读取票数剩余
                 remain = getTicketRemain(ticketInfo, flightCode)
                 # 如果该航班没有票跳过
                 if remain == 0:
+                    writeSystemLog(ItemMessage + u'已无余票!')
                     continue
                 # 获取dailyFlightId
                 dailyFlightId = getDailyFlightId(ticketInfo, flightCode)
                 if dailyFlightId == None:
+                    writeSystemLog(ItemMessage + u'无法获取航班Id，将跳过此项!')
                     continue
                 # 如果需票数小于实际剩余按实际票数抢
                 if need > remain:
+                    writeSystemLog(ItemMessage + u'余票不足，按实际票数抢!')
                     n = remain
                 else:
                     n = need
                 pitchResult = orderTicket(dailyFlightId, n)
                 if pitchResult:
                     # 抢票成功
-                    pass
+                    writeSystemLog(ItemMessage + u'抢票成功!!!')
                 else:
                     # 抢票失败
-                    pass
+                    writeSystemLog(ItemMessage + u'抢票执行失败，将跳过此项!')
             # 执行过抢票程序任务完成
-            return False #返回False表示程序应该终止
+            writeSystemLog(u'已完成刷票动作，将停止执行!')
+            return False  #返回False表示程序应该终止
         # 刷新完毕等待一段时间
+        writeSystemLog(u'完成一次刷新，将等待%d秒...' % normalWaitingSecond)
         time.sleep(normalWaitingSecond)
         # 检查当前时间是否超过中午12点，如果超过停止刷票
-        if datetime.strftime(datetime.now(),"%H:%M") >= '12:00':
-            return False #返回False表示程序应该终止
+        if datetime.strftime(datetime.now(), "%H:%M") >= timeToStop:
+            writeSystemLog(u'程序已执行到执行时间(%s),将停止执行!' % timeToStop)
+            return False  #返回False表示程序应该终止
     # 程序到这里表示登录信息已经丢失
+    writeSystemLog(u'登录信息丢失将尝试重新登录!')
     return True
 
 
@@ -196,14 +208,31 @@ def pitcherTask():
     '''
     抢票程序主过程
     '''
-    while True:
+    writeSystemLog(u'程序开始执行!')
+    loginErrorCount = 0
+    pitchLoopResult = True
+    while pitchLoopResult:
         try:
-            login(username, password)
-            pitchLoop()
+            loginResult = login(username, password)
+            if loginResult:
+                # 如果登录成功清空原来登录失败的记录
+                loginErrorCount = 0
+                writeSystemLog(u'登录成功!')
+                pitchLoopResult = pitchLoop()
+            else:
+                loginErrorCount += 1
+                if loginErrorCount > maxLoginTry:
+                    writeSystemLog(u'登录失败超过%d次，程序退出！' % maxLoginTry)
+                    return
+                else:
+                    writeSystemLog(u'登录失败!')
+                    writeSystemLog(u'等待%s秒... ...' % errorWaitingSecond)
+                    time.sleep(errorWaitingSecond)
         except Exception as e:
             writeSystemLog(u'出现异常:' + unicode(e))
-        writeSystemLog(u'等待3秒... ...')
-        time.sleep(3)
+            writeSystemLog(u'等待%s秒... ...' % errorWaitingSecond)
+            time.sleep(errorWaitingSecond)
+    writeSystemLog(u'程序执行结束！' % maxLoginTry)
 
 
 
