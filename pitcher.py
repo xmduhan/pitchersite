2cbb5e99402faab0329ac20ac2f23ab686d4fc19
xmduhan <xmduhan@gmail.com>
2014-11-25 22:34:38 +0800

    ...

--- pitcher.py
+++ pitcher.py
@@ -4,7 +4,7 @@
 import sys, os
 
 path = r'E:\pydev\pitchersite'  # 项目位置
-#path = r'/home/wx/pydev/pitchersite'  # 项目位置
+#path = r'/home/wx/pitchersite'  # 项目位置
 settings = "pitchersite.settings"
 sys.path.append(path)
 os.chdir(path)
